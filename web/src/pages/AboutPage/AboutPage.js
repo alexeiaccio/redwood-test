3a5af80089f927bd762717f4b5071c82cd2c3301
Aleksey Tukachev <atukachev@spbtv.com>
2020-03-20 14:55:16 +0300

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import MainLayout from 'src/layouts/MainLayout'
+
+const AboutPage = () => {
+  return (
+    <MainLayout>
+      <h1>AboutPage</h1>
+      <p>Poop</p>
+    </MainLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/IndexPage/IndexPage.js
@@ -0,0 +1,12 @@
+import MainLayout from 'src/layouts/MainLayout'
+
+const IndexPage = () => {
+  return (
+    <MainLayout>
+      <h1>IndexPage</h1>
+      <p>Hello</p>
+    </MainLayout>
+  )
+}
+
+export default IndexPage
